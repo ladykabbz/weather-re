6b88fb4ca9806365d2528b8c4af93e5974af5fe1
ladykabbz <kabiinicole@gmail.com>
2023-04-14 16:04:35 +0300

    react search engine

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Weather from "./weather";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <h1>Weather App</h1>
+      <Weather />
+      <h5>
+        <a href="" target="_blank">
+          Open-source code
+        </a>
+        , by nicole kabii from <a href=" #addlink ">She Codes</a>
+      </h5>
+    </div>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
