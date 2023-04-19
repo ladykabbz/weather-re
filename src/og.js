7ce3ed5a98e7007cc391cf56d21ea259f153c2af
ladykabbz <kabiinicole@gmail.com>
2023-04-19 17:44:26 +0300

    update weather api

--- src/og.js
+++ src/og.js
@@ -12,66 +12,68 @@ export default function Og() {
     <div className="main">
       <Container>
         <h1>Weather App</h1>
-        <Weather />
-        <Row>
-          <Col>
-            <p>
-              <strong>Lisbon</strong> <br />
-              Monday 13:50 <br /> Clear{" "}
-            </p>
-          </Col>
-        </Row>
+        <div className="Weather">
+          <Weather />
+          <Row>
+            <Col>
+              <p>
+                <strong> lisbon </strong> <br />
+                Monday 13:50 <br /> Clear{" "}
+              </p>
+            </Col>
+          </Row>
 
-        <br />
+          <br />
 
-        <Row>
-          <Col>
-            <h2>☀ 24C</h2>
-          </Col>
-          <Col>
-            <p>precipitation: 28%</p>
-            <p>Wind: 8km/h</p>
-          </Col>
-        </Row>
+          <Row>
+            <Col>
+              <h2>☀ 24C</h2>
+            </Col>
+            <Col>
+              <p>precipitation: 28%</p>
+              <p>Wind: 8km/h</p>
+            </Col>
+          </Row>
 
-        <br />
+          <br />
 
-        <Row className="forecast">
+          <Row className="forecast">
+            <hr />
+            <Col>
+              <strong>
+                Tuesday <br /> ☀ <br /> 19C
+              </strong>
+            </Col>
+            <Col>
+              <strong>
+                Wednesday <br /> ☀ <br /> 19C
+              </strong>
+            </Col>
+            <Col>
+              <strong>
+                Thursday <br /> ☀ <br /> 19C
+              </strong>
+            </Col>
+            <Col>
+              <strong>
+                Friday <br /> ☀ <br /> 19C
+              </strong>
+            </Col>
+            <Col>
+              <strong>
+                Saturday <br /> ☀ <br /> 19C
+              </strong>
+            </Col>
+          </Row>
           <hr />
-          <Col>
-            <strong>
-              Tuesday <br /> ☀ <br /> 19C
-            </strong>
-          </Col>
-          <Col>
-            <strong>
-              Wednesday <br /> ☀ <br /> 19C
-            </strong>
-          </Col>
-          <Col>
-            <strong>
-              Thursday <br /> ☀ <br /> 19C
-            </strong>
-          </Col>
-          <Col>
-            <strong>
-              Friday <br /> ☀ <br /> 19C
-            </strong>
-          </Col>
-          <Col>
-            <strong>
-              Saturday <br /> ☀ <br /> 19C
-            </strong>
-          </Col>
-        </Row>
-        <hr />
-        <br />
-        <h5>
-          <a href="https://github.com/ladykabbz/weather-re" target="blank">
-            Open-source code
-          </a>
-          ,by NICOLE KABII
-        </h5>
+          <br />
+          <h5>
+            <a href="https://github.com/ladykabbz/weather-re" target="blank">
+              Open-source code
+            </a>
+            ,by NICOLE KABII
+          </h5>
+        </div>
       </Container>
     </div>
   );
